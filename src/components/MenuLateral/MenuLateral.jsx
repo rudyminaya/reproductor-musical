3637ecdc6032d339d88602a95b2b7a5e06cc57a0
Rudy Minaya <rudy.minaya@copstone.net>
2021-01-14 10:48:30 -0500

    soluciando contenedores fixed

--- src/components/MenuLateral/MenuLateral.jsx
+++ src/components/MenuLateral/MenuLateral.jsx
@@ -5,20 +5,22 @@ import { TituloLista, TituloPrincipal } from '../titulos/titulos';
 export const MenuLateral = (props) => {
     return (
         <div className="menuLateral bg-dark-red-secondary h-full lg:w-80">
-            <img className="px-10" src={logo} />
-            <div className="menuLateral__library my-8">
-                <TituloPrincipal nombreTitulo="Mi Librería" />
-                <TituloLista nombreLista="Recientes" />
-                <TituloLista nombreLista="Artistas" />
-                <TituloLista nombreLista="Canciones" />
-                <TituloLista nombreLista="Estaciones" />
-            </div>
-            <div className="menuLateral__playlist">
-                <TituloPrincipal nombreTitulo="Playlist" />
-                <TituloLista nombreLista="Metal" />
-                <TituloLista nombreLista="Para bailar" />
-                <TituloLista nombreLista="Rock 90s" />
-                <TituloLista nombreLista="Baladas" />
+            <div className="fixed">
+                <img className="px-10 w-3/5" src={logo} />
+                <div className="menuLateral__library my-8">
+                    <TituloPrincipal nombreTitulo="Mi Librería" />
+                    <TituloLista nombreLista="Recientes" />
+                    <TituloLista nombreLista="Artistas" />
+                    <TituloLista nombreLista="Canciones" />
+                    <TituloLista nombreLista="Estaciones" />
+                </div>
+                <div className="menuLateral__playlist">
+                    <TituloPrincipal nombreTitulo="Playlist" />
+                    <TituloLista nombreLista="Metal" />
+                    <TituloLista nombreLista="Para bailar" />
+                    <TituloLista nombreLista="Rock 90s" />
+                    <TituloLista nombreLista="Baladas" />
+                </div>
             </div>
         </div>
     );
